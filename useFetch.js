85c9137dae217e68b9d6ec815446baecadae4e9b
Alex Levine <32104550+aAlexLevine@users.noreply.github.com>
2021-02-04 23:33:08 -0500

    stubs useFetch

--- useFetch.js
+++ useFetch.js
@@ -1,1 +1,13 @@
 //useFetch
+
+const useFetch = () => {
+  //manage logic and state for status, loading, etc
+    //avoid impossible state situations
+  //tailor to different implementations
+    //using dynamic queries
+    //dynamic api's
+  //abort fetch on unmount
+
+  //optionally memoize/cache data retrieved
+
+}
