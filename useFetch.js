27be97c4e2a540b53e31aba2b9d26db11ec129d2
Alex Levine <32104550+aAlexLevine@users.noreply.github.com>
2021-02-09 21:34:35 -0500

    Basic useFetch

--- useFetch.js
+++ useFetch.js
@@ -1,6 +1,22 @@
-//useFetch
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const useFetch = () => {
+const useFetch = (url, method) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (!url) return;
+    axios[method](url)
+      .then(({ data }) => setData(data))
+      .catch((err) => setError(err));
+  }, [url, method]);
+
+  return data;
+};
+
+export default useFetch;
+
+// TODO:
   //manage logic and state for status, loading, etc
     //avoid impossible state situations
   //tailor to different implementations
@@ -9,5 +25,3 @@ const useFetch = () => {
   //abort fetch on unmount
 
   //optionally memoize/cache data retrieved
-
-}
